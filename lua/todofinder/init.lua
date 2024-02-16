7ec4a2ca30c4058831320a0e16c709d37ec036e0
Macks_s <mlabrecque0924@gmail.com>
2024-02-16 09:59:15 -0500

    Quit Vim Command Easter Egg

--- lua/todofinder/init.lua
+++ lua/todofinder/init.lua
@@ -2,10 +2,13 @@ local M = {}
 
 function Todo()
 	print("TODO: implement")
+	local files = vim.api.nvim_exec("grep TODO -lr *")
+	print(files)
 end
 
 function M.setup()
-	vim.api.nvim_create_user_command("FindTodo", Todo, {})
+	vim.api.nvim_create_user_command("TodoFinder", Todo, {})
+	vim.api.nvim_create_user_command("QuitVim", function() vim.cmd("q") end, {})
 end
 
 return M
