10ccf4170efe50154c73c0b6e6ceaca8a94b8459
Macks_s <mlabrecque0924@gmail.com>
2024-02-16 11:17:00 -0500

    Working

--- lua/todofinder/init.lua
+++ lua/todofinder/init.lua
@@ -1,16 +1,20 @@
 local M = {}
 
 function Todo()
-    local files = vim.fn.systemlist("grep TODO -lr *")
+	local files = vim.fn.systemlist("grep TODO -lr *")
 
-    for _, file in ipairs(files) do
-        print(file)
-    end
+	if #files == 0 then
+		print("No TODOs found")
+		return
+	end
+
+	-- Open telescope with the files
+	require('telescope.builtin').find_files({ cwd = vim.fn.getcwd(), prompt_title = "TODOs", find_command = { 'grep', '-rl', 'TODO', '.' } })
 end
 
 function M.setup()
-    vim.api.nvim_create_user_command("TodoFinder", Todo, {})
-    vim.api.nvim_create_user_command("QuitVim", function() vim.cmd("q") end, {})
+	vim.api.nvim_create_user_command("TodoFinder", Todo, {})
+	vim.api.nvim_create_user_command("QuitVim", function() vim.cmd("q") end, {})
 end
 
 return M
