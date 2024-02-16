254ebb491d5b68634fea2380040a09ad01bc57d1
Macks_s <mlabrecque0924@gmail.com>
2024-02-16 10:20:33 -0500

    Array of file names

--- lua/todofinder/init.lua
+++ lua/todofinder/init.lua
@@ -1,13 +1,16 @@
 local M = {}
 
 function Todo()
-	local files = vim.cmd("grep TODO -lr * --exclude='node_modules/*'")
-	print(files)
+    local files = vim.fn.systemlist("grep TODO -lr *")
+
+    for _, file in ipairs(files) do
+        print(file)
+    end
 end
 
 function M.setup()
-	vim.api.nvim_create_user_command("TodoFinder", Todo, {})
-	vim.api.nvim_create_user_command("QuitVim", function() vim.cmd("q") end, {})
+    vim.api.nvim_create_user_command("TodoFinder", Todo, {})
+    vim.api.nvim_create_user_command("QuitVim", function() vim.cmd("q") end, {})
 end
 
 return M
